5321e752a6e0de2864e22091862e1f2fd197feef
Dunissimo <SuperDen214@yandex.ru>
2021-05-24 22:35:17 +0400

    Fourth commit. Big changes

--- script.js
+++ script.js
@@ -10,15 +10,39 @@ let personalMovieDB = {
   privat: false,
 };
 
-let nameOfFirstFilm = prompt("Один из недавно просмотренных фильмов?");
-let ratingOfFirstFilm = prompt("Оцените его по 10 бальной шкале");
+//let nameOfFirstFilm = prompt("Один из недавно просмотренных фильмов?");
+//let ratingOfFirstFilm = prompt("Оцените его по 10 бальной шкале");
 
-personalMovieDB.movies[nameOfFirstFilm] = ratingOfFirstFilm;
+//personalMovieDB.movies[nameOfFirstFilm] = ratingOfFirstFilm;
 
-let nameOfSecondFilm = prompt("Один из недавно просмотренных фильмов?");
-let ratingOfSecondFilm = prompt("Оцените его по 10 бальной шкале");
+//let nameOfSecondFilm = prompt("Один из недавно просмотренных фильмов?");
+//let ratingOfSecondFilm = prompt("Оцените его по 10 бальной шкале");
 
-personalMovieDB.movies[nameOfSecondFilm] = ratingOfSecondFilm;
+//personalMovieDB.movies[nameOfSecondFilm] = ratingOfSecondFilm;
 
+//console.log(personalMovieDB);
+//console.log(personalMovieDB.movies);
+
+for (let i = 0; i < personalMovieDB.count; i++) {
+    let nameOfFilm = prompt("Один из недавно просмотренных фильмов?");
+    let ratingOfFilm = prompt("Оцените его по 10 бальной шкале");
+    
+
+    if (nameOfFilm != null && ratingOfFilm != null && nameOfFilm != '' && ratingOfFilm != '' && nameOfFilm.length < 50) {
+      personalMovieDB.movies[nameOfFilm] = ratingOfFilm;   
+    } else {
+      console.log("Error");
+      i--;
+    }
+    if (personalMovieDB.count <= 10) {
+      console.log("Мало");
+    } else if (personalMovieDB.count > 10 && personalMovieDB < 30) {
+      console.log("Норм");
+    } else if (personalMovieDB.count >= 30){
+      console.log("Много");
+    } else {
+      console.log("ERROR");
+    }
+    
+}
 console.log(personalMovieDB);
-console.log(personalMovieDB.movies);
