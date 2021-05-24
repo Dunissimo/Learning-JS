42be810a86bb4347e3cd894e183d55c0ac1d38db
Dunissimo <SuperDen214@yandex.ru>
2021-05-24 20:55:51 +0400

    Second commit. I did the first task

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
 "use strict";
 
+let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?");
+
+let personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+let nameOfFirstFilm = prompt("Один из недавно просмотренных фильмов?");
+let ratingOfFirstFilm = prompt("Оцените его по 10 бальной шкале");
+
+personalMovieDB.movies[nameOfFirstFilm] = ratingOfFirstFilm;
+
+let nameOfSecondFilm = prompt("Один из недавно просмотренных фильмов?");
+let ratingOfSecondFilm = prompt("Оцените его по 10 бальной шкале");
+
+personalMovieDB.movies[nameOfSecondFilm] = ratingOfSecondFilm;
+
+console.log(personalMovieDB);
+console.log(personalMovieDB.movies);
